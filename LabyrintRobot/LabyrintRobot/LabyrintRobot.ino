2a3e7f0144e8e7c8b9f92660b907bb317cc9ffb0
Sigurd <Sigurd@LAPTOP-2I4OVDA6>
2018-01-31 18:40:45 +0100

    har lige skrevet jeff er sej

--- LabyrintRobot/LabyrintRobot/LabyrintRobot.ino
+++ LabyrintRobot/LabyrintRobot/LabyrintRobot.ino
@@ -6,7 +6,7 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-//dette er en setup funktion jeff
+//dette er en setup funktion jeff er sej
 }
 
 // the loop function runs over and over again until power down or reset
