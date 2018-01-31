987272bd65cc4e661df180cb8546fcd344049c06
Sigurd <Sigurd@LAPTOP-2I4OVDA6>
2018-01-31 18:24:54 +0100

    hej jeg er MASTER

---
+++ LabyrintRobot/LabyrintRobot/LabyrintRobot.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		LabyrintRobot.ino
+ Created:	1/31/2018 6:18:27 PM
+ Author:	Sigurd
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
